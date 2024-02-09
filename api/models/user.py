958b6679d13aeda93fe8be6f801a51988c26609c
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-09 12:19:26 +0900

    feat: :card_file_box: Add non-null constraints to email and password columns in User model

--- api/models/user.py
+++ api/models/user.py
@@ -8,8 +8,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), unique=True)
+    email = Column(String(255), unique=True, nullable=False)
     nickname = Column(String(64))
-    password = Column(String(64))
+    password = Column(String(64), nullable=False)
 
     tasks = relationship("Task", back_populates="user", cascade="delete")
