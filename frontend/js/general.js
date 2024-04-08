49267f28ada9cc96a3a02b35a3dba17e3486beeb
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-04-08 14:38:03 +0900

    refactor: :recycle: Refactor API calls in frontend JavaScript files

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,18 +1,6 @@
-const API_HOST = 'http://localhost:8000'
-
 const checkLogin = () => {
   if (localStorage.getItem('token')) {
     return true
   }
   return false
-}
-
-const logout = () => {
-  localStorage.removeItem('token')
-  location.href = '/login.html'
-}
-
-const handleLoginError = () => {
-  alert('ログイン情報が確認できませんでした、ログインページに移動します')
-  logout()
 }
