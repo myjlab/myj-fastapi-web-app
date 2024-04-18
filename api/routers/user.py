46f2340de2ce0c16bd8f8a71a4d6732cceb39bfa
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-04-18 20:52:34 +0900

    docs: :memo: Add comments for API router

--- api/routers/user.py
+++ api/routers/user.py
@@ -15,6 +15,7 @@ def create_user(
     db: Session = Depends(get_db),
 ):
     db_user = user_crud.get_user_by_email(db, email=user.email)
+    # すでに登録されているメールアドレスの場合
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return user_crud.create_user(db, user)
